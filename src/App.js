85ed072f91192a90e2f805a3bd28308ae4993192
vivek <vivektalluri94@gmail.com>
2024-08-13 20:21:55 +0530

    india

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 function App() {
   return (
     <h1>
-      <spam className="yellow">Advance</spam><br></br>
+      <spam className="yellow">Advance</spam><br/>
       <span className="saffron">HAPPY</span><br/>
       <span className="white">INDEPENDENCE</span><br/>
-      <span className="green">DAY</span><br></br>
-      <spam className="saffron">77th independence day</spam><br></br>
+      <span className="green">DAY</span><br/>
+      <spam className="saffron">77th independence day</spam><br/>
       <spam className="blue">jai Hind</spam>
     </h1>
   );
