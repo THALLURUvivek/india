392990619917e819775e68dd89e883f08dd47699
vivek <vivektalluri94@gmail.com>
2024-08-13 19:18:32 +0530

    india

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1>
+      <span className="saffron">I</span>
+      <span className="white">N</span>
+      <span className="green">D</span>
+      <span className="saffron">I</span>
+      <span className="white">A</span>
+    </h1>
+  );
+}
+
+export default App;
